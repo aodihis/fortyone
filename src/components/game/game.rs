b1fbbe54e15b8c69013e05f0bfdb2a23e5de40af
Iqbal <aodihis@gmail.com>
2025-01-12 20:50:11 +0700

    Add card distributing add starting of game.

--- src/components/game/game.rs
+++ src/components/game/game.rs
@@ -1,6 +1,6 @@
 use std::iter::Iterator;
 use leptos::{component, view, IntoView};
-use leptos::prelude::{ClassAttribute, ElementChild, ReadSignal, StyleAttribute};
+use leptos::prelude::{signal, ClassAttribute, ElementChild, Get, ReadSignal, StyleAttribute};
 
 enum EnemyPos {
     Left,
@@ -9,10 +9,11 @@ enum EnemyPos {
 }
 #[component]
 pub fn Game() -> impl IntoView {
-    let deck_total: u8 = 52;
+    let (deck, set_deck) = signal(52);
     view! {
         <div class="game-container">
-            <Deck total={deck_total}/>
+            <CardDistribute/>
+            <Deck total={deck}/>
             <CurrentPlayer/>
             <Enemy position={EnemyPos::Left}/>
             <Enemy position={EnemyPos::Right}/>
@@ -22,17 +23,46 @@ pub fn Game() -> impl IntoView {
 }
 
 #[component]
-fn Deck(total: u8) -> impl IntoView {
+fn CardDistribute() -> impl IntoView {
+    view! {
+        <>
+            <div class="starting-card card card-back" style="animation: throw-card-bottom 1s ease-in-out forwards;"></div>
+            <div class="starting-card card card-back" style="animation: throw-card-right 1s ease-in-out 1s forwards;"></div>
+            <div class="starting-card card card-back" style="animation: throw-card-top 1s ease-in-out 2s forwards;"></div>
+            <div class="starting-card card card-back" style="animation: throw-card-left 1s ease-in-out 3s forwards;"></div>
+            <div class="starting-card card card-back" style="animation: throw-card-bottom 1s ease-in-out 4s forwards;"></div>
+            <div class="starting-card card card-back" style="animation: throw-card-right 1s ease-in-out 5s forwards;"></div>
+            <div class="starting-card card card-back" style="animation: throw-card-top 1s ease-in-out 6s forwards;"></div>
+            <div class="starting-card card card-back" style="animation: throw-card-left 1s ease-in-out 7s forwards;"></div>
+            <div class="starting-card card card-back" style="animation: throw-card-bottom 1s ease-in-out 8s forwards;"></div>
+            <div class="starting-card card card-back" style="animation: throw-card-right 1s ease-in-out 9s forwards;"></div>
+            <div class="starting-card card card-back" style="animation: throw-card-top 1s ease-in-out 10s forwards;"></div>
+            <div class="starting-card card card-back" style="animation: throw-card-left 1s ease-in-out 11s forwards;"></div>
+            <div class="starting-card card card-back" style="animation: throw-card-bottom 1s ease-in-out 12s forwards;"></div>
+            <div class="starting-card card card-back" style="animation: throw-card-right 1s ease-in-out 13s forwards;"></div>
+            <div class="starting-card card card-back" style="animation: throw-card-top 1s ease-in-out 14s forwards;"></div>
+            <div class="starting-card card card-back" style="animation: throw-card-left 1s ease-in-out 15s forwards;"></div>
+
+        </>
+
+    }
+}
+#[component]
+fn Deck(total: ReadSignal<u8>) -> impl IntoView {
     view! {
         <div class="deck">
-                {(0..total).into_iter()
-                    .map(|n:u8| {
-                    let tr = n/4;
-                    view! {<div class="card card-back" style=format!("transform: translate(-{tr}px, -{tr}px);")></div>}
-                })
-                    .collect::<Vec<_>>()
+            {(0..total.get()).into_iter()
+                .map(|n:u8| {
+                let tr = n/4;
+                let mut class = "card card-back";
+                if n == 51 {
+                    class = "card card-back card-throw";
                 }
-            </div>
+                view! {<div class={class} style=format!("transform: translate(-{tr}px, -{tr}px);")></div>}
+            })
+                .collect::<Vec<_>>()
+            }
+        </div>
     }
 }
 #[component]
